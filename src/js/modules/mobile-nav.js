38722828873c51577190d1f3017ebe6cf8bf7b64
yargrinders <yargrindersp20@gmail.com>
2024-08-12 21:58:02 +0200

    add_new_burger

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,74 @@
-function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+// Module Burger
+// function mobileNav() {
+//     function menuInit() {
+// 		const iconMenu = document.querySelector(".icon-menu");
+	  
+// 		if (iconMenu) {
+// 		  iconMenu.addEventListener("click", function (e) {
+// 			e.preventDefault();
+// 			document.documentElement.classList.toggle("menu-open");
+// 		  });
+// 		}
+// 	  }
+	  
+// 	  function menuOpen() {
+// 		document.documentElement.classList.add("menu-open");
+// 	  }
+	  
+// 	  function menuClose() {
+// 		document.documentElement.classList.remove("menu-open");
+// 	  }
+	  
+// 	  // Initialize menu
+// 	  document.addEventListener("DOMContentLoaded", function () {
+// 		menuInit();
+// 	  });
+// }
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+// export default mobileNav;
+
+// Module Burger
+function mobileNav() {
+    function menuInit() {
+        const iconMenu = document.querySelector(".icon-menu");
+      
+        if (iconMenu) {
+            iconMenu.addEventListener("click", function (e) {
+                e.preventDefault();
+                document.documentElement.classList.toggle("menu-open");
+                
+                // Disable / Enable - Burger page Scroll
+                if (document.documentElement.classList.contains("menu-open")) {
+                    disableScroll();
+                } else {
+                    enableScroll();
+                }
+            });
+        }
+    }
+    
+    function disableScroll() {
+        document.body.classList.add("no-scroll");
+    }
+    
+    function enableScroll() {
+        document.body.classList.remove("no-scroll");
+    }
+    
+    function menuOpen() {
+        document.documentElement.classList.add("menu-open");
+        disableScroll();
+    }
+    
+    function menuClose() {
+        document.documentElement.classList.remove("menu-open");
+        enableScroll();
+    }
+    
+    // Initialize menu
+    document.addEventListener("DOMContentLoaded", function () {
+        menuInit();
+    });
 }
 
 export default mobileNav;
